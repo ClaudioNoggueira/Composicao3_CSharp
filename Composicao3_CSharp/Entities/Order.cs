c93d03af56a57a77243a0e8b9f4861bf5e89a0b6
Claudio Nogueira <cllaudionoggueira@gmail.com>
2020-12-27 17:06:33 -0300

    First commit

---
+++ Composicao3_CSharp/Entities/Order.cs
@@ -0,0 +1,73 @@
+﻿using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities {
+    class Order {
+        public DateTime Moment { get; set; }
+        public Client Client { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() {
+
+        }
+
+        public Order(DateTime moment, Client client, OrderStatus status) {
+            Moment = moment;
+            Client = client;
+            Status = status;
+        }
+
+        public void AddItem(OrderItem ordemItem) {
+            Items.Add(ordemItem);
+        }
+
+        public void RemoveItem(OrderItem orderItem) {
+            Items.Remove(orderItem);
+        }
+
+        public double Total() {
+            double total = 0;
+            foreach (OrderItem item in Items) {
+                total += item.SubTotal();
+            }
+            return total;
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY");
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status.ToString());
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Items) {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: R$ " + Total().ToString("F2"));
+            return sb.ToString();
+        }
+
+
+
+    }
+    /*
+     * 
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+     */
+
+
+}
---
+++ Composicao3_CSharp/Program.cs
@@ -0,0 +1,53 @@
+﻿using Entities;
+using Entities.Enums;
+using System;
+
+namespace Composicao3_CSharp {
+    class Program {
+        static void Main(string[] args) {
+            string data = DateTime.Now.ToLongDateString();
+            Console.WriteLine(data + "\n\n");
+
+            Console.WriteLine("Enter client data:");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("\nEnter order data:");
+            DateTime moment = DateTime.Parse(DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString());
+
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Console.Write("\nHow many items to this order? ");
+            int itemsQtd = int.Parse(Console.ReadLine());
+
+            Order order = new Order(moment, client, status);
+
+            for (int i = 1; i <= itemsQtd; i++) {
+                Console.WriteLine("\nEnter " + i + "º item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int productQtd = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice);
+
+                OrderItem orderItem = new OrderItem(productQtd, productPrice, product);
+
+                order.AddItem(orderItem);
+            }
+            Console.WriteLine("\n" + order);
+        }
+    }
+}
